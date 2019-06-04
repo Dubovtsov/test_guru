0d1cbb44a9bd14601156f51cade31f0458dfa541
Dubwin <dubwin@yandex.ru>
2019-06-04 23:45:04 +0300

    def sort_by_category

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,13 @@
 class Test < ApplicationRecord
   belongs_to :category
+
+  def self.sort_by_category(category)
+    Test.where(category_id: category)
+  end
 end
+
+# Создайте метод класса в модели Test, 
+# который возвращает отсортированный 
+# по убыванию массив названий всех Тестов 
+# у которых Категория называется определённым 
+# образом (название категории передается в метод в качестве аргумента).
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(login: 'Ivan', email: 'dubwin@yandex.ru')
+
+categories = [:Ruby, :Rails, :JS, :HTML, :CSS]
+categories.each { |c| Category.create(title: c) }
+
+Test.create(title: 'Введение в Ruby', category_id: 1,)
+Test.create(title: 'Идиомы Ruby', category_id: 1,)
+Test.create(title: 'Миграции в Rails', level: 1, category_id: 2,)
+Test.create(title: 'Генераторы Rails', category_id: 2,)
+
+Question.create(body: "Является ли Ruby объектно-ориентированным языком программирования", test_id: 1)
+
+Answer.create(body: 'Да', correct: true, question_id: 1)
+Answer.create(body: 'Нет', correct: false, question_id: 1)
