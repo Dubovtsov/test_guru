e7ffc3f47cfe879e7e263276b3b11aabfb0f50c8
Dubwin <dubwin@yandex.ru>
2019-06-06 23:24:53 +0300

    fix

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,6 +3,6 @@ class Test < ApplicationRecord
   has_many :completed_tests
 
   def self.sort_by_category(category)
-    Test.select(:title).joins(:category).where(categories: { title: category }).order(title: :desc)
+    Test.pluck(:title).joins(:category).where(categories: { title: category }).order(title: :desc)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(login: 'Ivan', email: 'dubwin@yandex.ru')
+user = User.create(login: 'Ivan', email: 'dubwin@yandex.ru')
 
 categories = [:Ruby, :Rails, :JS, :HTML, :CSS]
 categories.each { |c| Category.create(title: c) }
+categories
 
-Test.create(title: 'Введение в Ruby', category_id: 1,)
-Test.create(title: 'Идиомы Ruby', category_id: 1,)
-Test.create(title: 'Миграции в Rails', level: 1, category_id: 2,)
-Test.create(title: 'Генераторы Rails', category_id: 2,)
+categories[0].id.tests.create!(title: 'Введение в Ruby')
+
+# Test.create(title: 'Введение в Ruby', category_id: categories[0].id,)
+Test.create(title: 'Идиомы Ruby', category_id: categories[0].id,)
+Test.create(title: 'Миграции в Rails', level: 1, category_id: categories[1].id,)
+Test.create(title: 'Генераторы Rails', category_id: categories[1].id,)
 
 Question.create(body: "Является ли Ruby объектно-ориентированным языком программирования", test_id: 1)
 
