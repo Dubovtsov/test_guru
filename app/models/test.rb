78c75bf47ce854dd3124bd3b398a60dfb7e7ed3f
Dubovtsov <dubwin@yandex.ru>
2019-06-11 05:11:53 -0700

    author - authored_tests

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,8 +3,7 @@ class Test < ApplicationRecord
   has_many :questions
   has_many :tests_users
   has_many :users, through: :tests_users
-  alias_attribute :author, :user
-  belongs_to :user
+  belongs_to :author, class_name: 'User'
 
   def self.sort_by_category(category)
     Test.pluck(:title).joins(:category).where(categories: { title: category }).order(title: :desc)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   has_many :tests_users
   has_many :categories
-  alias_attribute :created_tests, :tests
-  has_many :created_tests, class_name: 'Test', foreign_key: 'user_id'
+  has_many :authored_tests, class_name: 'Test'
   has_many :tests, through: :tests_users
   has_many :questions
   has_many :answer
