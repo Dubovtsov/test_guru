76be7f338dffaffc6ebc53e3761cedb8e5430cc8
Dubwin <dubwin@yandex.ru>
2019-06-26 00:48:59 +0300

    helpers-2

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,5 +1,5 @@
 module QuestionsHelper
-  def question_header
-    
+  def question_header(test_title)
+    render inline: "Create New #{test_title} Question" if action = :new || "Edit #{test_title} Question"
   end
 end
