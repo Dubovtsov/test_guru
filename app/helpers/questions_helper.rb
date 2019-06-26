21d014f730c890f2e781121558851947e19e827e
Dubovtsov <dubwin@yandex.ru>
2019-06-26 02:00:15 -0700

    for checking

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,5 +1,11 @@
 module QuestionsHelper
+
   def question_header(test_title)
-    render inline: "Create New #{test_title} Question" if action = :new || "Edit #{test_title} Question"
+    if current_page?(new_test_question_path)
+      render inline: "Create New #{test_title} Question"
+    else
+      "Edit #{test_title} Question"
+    end
+    # render inline: current_page?(new_test_question_path) ? "Create New #{test_title} Question" : "Edit #{test_title} Question"
   end
 end
