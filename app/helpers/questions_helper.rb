5eb08cca1e314ad6dac0f9f84e1d968a5f6a7d6b
Dubovtsov <dubwin@yandex.ru>
2019-06-26 07:30:23 -0700

    form

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -2,7 +2,7 @@ module QuestionsHelper
 
   def question_header(test_title)
     if current_page?(new_test_question_path)
-      render inline: "Create New #{test_title} Question"
+      "Create New #{test_title} Question"
     else
       "Edit #{test_title} Question"
     end
