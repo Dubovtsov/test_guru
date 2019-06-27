53e4bf09f6b13822a2bf54052f7d978ba248e2b6
Dubovtsov <dubwin@yandex.ru>
2019-06-27 15:02:25 +0300

    lesson_9

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tests#index'
 
   resources :tests do
-    resources :questions
+    resources :questions, shallow: true do
+      resources :answers, shallow: true
+    end
   end
 
 end
--- db/migrate/20190627065503_create_answers.rb
+++ db/migrate/20190627065503_create_answers.rb
@@ -1,9 +1,7 @@
 class CreateAnswers < ActiveRecord::Migration[5.2]
   def change
     create_table :answers do |t|
-      t.text :body, null: false
-      t.boolean :correct, default: false
-      t.references :question, foreign_key: true
+      t.text :body
 
       t.timestamps
     end
