4a03848cc54376e9f2d48d25bd086e92d74a226d
Dubovtsov <dubwin@yandex.ru>
2019-06-28 16:34:31 +0300

    model test_passages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  has_many :tests_users
+  has_many :test_passages
   has_many :categories
   has_many :authored_tests, class_name: 'Test', foreign_key: :author_id
-  has_many :tests, through: :tests_users
+  has_many :tests, through: :test_passeges
   has_many :questions
   has_many :answer
 
