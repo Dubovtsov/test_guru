5989387974bdb7f92f97effce4887eb643a1cb04
Dubwin <dubwin@yandex.ru>
2019-07-10 06:38:11 +0300

    auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+
   has_many :test_passages
   has_many :categories
   has_many :authored_tests, class_name: 'Test', foreign_key: :author_id
@@ -7,6 +8,7 @@ class User < ApplicationRecord
   has_many :answer
 
   validates :email, presence: true
+  has_secure_password
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
@@ -14,16 +16,6 @@ class User < ApplicationRecord
 
   def tests_by_level(level)
     Test.where(level: level).includes(:tests_users).where(tests_users: { user_id: self.id })
-  end
-
-  def authenticate(password_string)
-    digest(password_string) == self.password_digest ? self : false
-  end
-
-  private
-
-  def digest(string)
-    Digest::SHA1.hexdigest(string)
   end
 
 end
