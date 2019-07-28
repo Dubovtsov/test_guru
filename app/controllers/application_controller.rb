d748e08574147b5d71f05b2ba49be46e8394673b
Dubwin <dubwin@yandex.ru>
2019-07-28 21:53:46 +0300

    lang default

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < ActionController::Base
 
+  before_action :set_locale
+
   def after_sign_in_path_for(resource)
     flash[:success] = "Hello, #{current_user.first_name}"
     resource.admin? ? admin_tests_path : root_path
-   end
+  end
+
+  def default_url_options
+    { lang: I18n.locale }
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
+  end
 end
