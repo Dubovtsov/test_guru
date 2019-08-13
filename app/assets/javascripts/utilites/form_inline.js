9ad46132bddd83ef07d04f921331bbe0456856e4
Dubwin <dubwin@yandex.ru>
2019-08-13 23:16:03 +0300

    jquery

--- app/assets/javascripts/utilites/form_inline.js
+++ app/assets/javascripts/utilites/form_inline.js
@@ -1,10 +1,12 @@
 document.addEventListener('turbolinks:load', function() {
-  var controls = document.querySelectorAll('.form-inline-link')
 
-  if (controls.length) {
-    for (var i = 0; i < controls.length; i++) {
-      controls[i].addEventListener('click', formInlineLinkHandler)
-    }
+  $('.form-inline-link').on('click', formInlinLinkHandler)
+
+  var errors = document.querySelector('.resource-errors')
+
+  if (errors) {
+    var resourceId = errors.dataset.resourceId
+    formInlineHandler(resourceId)
   }
 })
 
@@ -17,12 +19,15 @@ function formInlineLinkHandler(event) {
 
 function formInlineHandler(testId) {
   var link = document.querySelector('.form-inline-link[data-test-id="' + testId + '"]')
-  var testTitle = document.querySelector('.test-title[data-test-id="' + testId + '"]')
-  var formInline = document.querySelector('.form-inline[data-test-id="' + testId + '"]')
+  var $testTitle = $('.test-title[data-test-id="' + testId + '"]')
+  var $formInlineHandlere = $('.form-inline[data-test-id="' + testId + '"]')
 
-  if (formInline.classList.contains('hide')) {
+  $formInline.toggle()
+  $testTitle.toggle()
 
+  if ($formInline.is(':visible')) {
+    link.textContent = 'Cancel'
   } else {
-    
+    link.textContent = 'Edit'
   }
 }
