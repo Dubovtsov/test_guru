d5cdc26fba124a0f22bfef1ead3d64478a93f91e
Dubovtsov <dubwin@yandex.ru>
2019-10-10 01:29:50 +0300

    its work! badge

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -13,10 +13,7 @@ class TestPassagesController < ApplicationController
     if @test_passage.completed?
       TestsMailer.compleated_test(@test_passage).deliver_now
 
-        @category = @test_passage.test.category
-# Создать метод сравнивающий количество пройденных тестов 
-# с их общим количеством в категории
-        current_user.badges.push(@badge)
+      reward(@test_passage)
 
 
       redirect_to result_test_passage_path(@test_passage)
@@ -44,6 +41,24 @@ class TestPassagesController < ApplicationController
   end
 
   private
+
+  def reward(test_passage)
+    @category = test_passage.test.category.id
+    # Создать метод сравнивающий количество пройденных тестов 
+    # с их общим количеством в категории
+    
+    @all_tests_in_the_category = Test.all.where(category_id: @category).count
+    @user_test_passages = current_user.tests.joins(:test_passages).where(category_id: @category).distinct!.count
+
+    if @user_test_passages == 0
+      @badge = Badge.where("category_id = ? AND badge_rule_id = ?", @category, 3).take
+      current_user.badges.push(@badge)
+    elsif @user_test_passages == @all_tests_in_the_category
+      @badge = Badge.where("category_id = ? AND badge_rule_id = ?", @category, 2).take
+      current_user.badges.push(@badge)
+    else
+    end
+  end
 
   def set_test_passage
     @test_passage = TestPassage.find(params[:id])
--- app/models/badge_rule.rb
+++ app/models/badge_rule.rb
@@ -1,3 +1,2 @@
 class BadgeRule < ApplicationRecord
-  
 end
