f3117bc56d0cc16371bf0c2e1ed68533bd115a6e
Dubwin <dubwin@yandex.ru>
2019-10-29 00:29:19 +0300

    successful_tests

--- app/models/badge_rule.rb
+++ app/models/badge_rule.rb
@@ -7,17 +7,17 @@ class BadgeRule < ApplicationRecord
     @all_tests_in_the_category = Test.all.where(category_id: @category).count
     @user_test_passages = @current_user.test_passages.where(test_id: Test.where(category_id: @category))
 
-    @successful_test ||= 0
+    @successful_tests = []
 
     @user_test_passages.each do |test_passage|
-      @successful_test += 1 if test_passage.successfully?
+      @successful_tests.push(test_passage) if test_passage.successfully?
     end
 
-    if @successful_test == 1
+    if @successful_tests.size == 1
       @badge = Badge.where("category_id = ? AND badge_rule_id = ?", @category, 2).take
       current_user.badges.push(@badge) if @badge.present?
       # the_first_test_in_the_category(@category, @current_user)
-    elsif @successful_test == @all_tests_in_the_category && @user_test_passages.count == @successful_test
+    elsif @successful_tests == @all_tests_in_the_category && @user_test_passages.count == @successful_tests
       @badge = Badge.where("category_id = ? AND badge_rule_id = ?", @category, 1).take
       current_user.badges.push(@badge) if @badge.present?
       # all_tests_in_the_category(@category, @current_user)
