8ed4dc012a8c45d907c177d230067d7a286b2da9
Dubwin <dubwin@yandex.ru>
2019-11-15 01:05:18 +0300

    timer not work

--- app/assets/javascripts/utilites/timer.js
+++ app/assets/javascripts/utilites/timer.js
@@ -1,35 +1,37 @@
 document.addEventListener('turbolinks:load', function() {
-  var interval;
+  var control = document.getElementById('test-passage-form')
+  if (control) {
+    var interval;
+// счетчик не должен обнуляться при переходе на следующий вопрос или обновлении страницы
+    function countdown() {
+      clearInterval(interval);
 
-  function countdown() {
-    clearInterval(interval);
+      interval = setInterval( function() {
+        var timer = localStorage.getItem("time") || $('.test-timer').html();
+        // localStorage.getItem("time") ||
+        timer = timer.split(':');
+        var minutes = timer[0];
+        var seconds = timer[1];
+        seconds -= 1;
 
-    interval = setInterval( function() {
-      var timer = localStorage.getItem("time") || $('.test-timer').html();
-      // localStorage.getItem("time") ||
-      timer = timer.split(':');
-      var minutes = timer[0];
-      var seconds = timer[1];
+        if (minutes < 0) return;
+        else if (seconds < 0 && minutes != 0) {
+          minutes -= 1;
+          seconds = 59;
+        }
+        else if (seconds < 10 && length.seconds != 2) seconds = '0' + seconds;
 
-      seconds -= 1;
-
-      if (minutes < 0) return;
-      else if (seconds < 0 && minutes != 0) {
-        minutes -= 1;
-        seconds = 59;
-      }
-      else if (seconds < 10 && length.seconds != 2) seconds = '0' + seconds;
-
-      $('.test-timer').html(minutes + ':' + seconds);
-      localStorage.setItem('time', minutes + ':' + seconds);
-      if (minutes == 0 && seconds == 0) {
-        clearInterval(interval);
-        submitForm()
-      }
-    }, 1000);
+        $('.test-timer').html(minutes + ':' + seconds);
+        if (minutes == 0 && seconds == 0) {
+          clearInterval(interval);
+          submitForm()
+        }
+        localStorage.setItem('time', minutes + ':' + seconds);
+      }, 1000);
+    }
+    countdown();
   }
-  countdown();
-});
+})
 
 function submitForm() {
   var formTag = document.querySelector('#test-passage-form')
