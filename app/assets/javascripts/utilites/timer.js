0336bd0f50976c5789b9863827619121292f3b5b
Dubwin <dubwin@yandex.ru>
2019-11-15 00:49:01 +0300

    querySelector

--- app/assets/javascripts/utilites/timer.js
+++ app/assets/javascripts/utilites/timer.js
@@ -1,18 +1,18 @@
 document.addEventListener('turbolinks:load', function() {
   var interval;
 
-  // var sec=localStorage.getItem("time") || 10;
-
   function countdown() {
     clearInterval(interval);
 
     interval = setInterval( function() {
-      var timer = $('.test-timer').html();
-      // localStorage.getItem("time") || 
+      var timer = localStorage.getItem("time") || $('.test-timer').html();
+      // localStorage.getItem("time") ||
       timer = timer.split(':');
       var minutes = timer[0];
       var seconds = timer[1];
+
       seconds -= 1;
+
       if (minutes < 0) return;
       else if (seconds < 0 && minutes != 0) {
         minutes -= 1;
@@ -21,7 +21,7 @@ document.addEventListener('turbolinks:load', function() {
       else if (seconds < 10 && length.seconds != 2) seconds = '0' + seconds;
 
       $('.test-timer').html(minutes + ':' + seconds);
-      // localStorage.setItem('time', minutes + ':' + seconds);
+      localStorage.setItem('time', minutes + ':' + seconds);
       if (minutes == 0 && seconds == 0) {
         clearInterval(interval);
         submitForm()
