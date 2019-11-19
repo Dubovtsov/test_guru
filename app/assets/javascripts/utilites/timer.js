4417d48f09ead61964778905e29a50df845fd594
Dubovtsov <dubwin@yandex.ru>
2019-11-19 15:29:46 +0300

    timer is over

--- app/assets/javascripts/utilites/timer.js
+++ app/assets/javascripts/utilites/timer.js
@@ -1,36 +1,45 @@
 document.addEventListener('turbolinks:load', function() {
-sessionStorage.clear();
   var control = document.querySelector('.test-timer')
   if (control) {
-    var interval;
-// счетчик не должен обнуляться при переходе на следующий вопрос или обновлении страницы
-    function countdown() {
-      clearInterval(interval);
+    var interval
 
+    function countdown() {
+      clearInterval(interval)
       interval = setInterval( function() {
-        var timer = sessionStorage.getItem('time') || $('.test-timer').html();
-        // localStorage.getItem("time") ||
-        timer = timer.split(':');
-        var minutes = timer[0];
-        var seconds = timer[1];
-        seconds -= 1;
+        var timer = sessionStorage.getItem('time') || $('.test-timer').html()
+        timer = timer.split(':')
+        var minutes = timer[0]
+        var seconds = timer[1]
+
+        if (minutes == 0 && seconds == 0){
+          console.log(sessionStorage.getItem('time'))
+          clearInterval(interval)
+          submitForm()
+          return false
+        }
+        else if (minutes !== 0 && seconds !== 0) {
+          seconds -= 1
+        }
 
         if (minutes < 0) return;
         else if (seconds < 0 && minutes != 0) {
           minutes -= 1;
           seconds = 59;
         }
-        else if (seconds < 10 && length.seconds != 2) seconds = '0' + seconds;
+        else if (seconds < 10 && length.seconds != 2) seconds = '0' + seconds
+        sessionStorage.setItem('time', minutes + ':' + seconds)
 
-        $('.test-timer').html(minutes + ':' + seconds);
+        $('.test-timer').html(minutes + ':' + seconds)
         if (minutes == 0 && seconds == 0) {
-          clearInterval(interval);
+          clearInterval(interval)
           submitForm()
         }
-        sessionStorage.setItem('time', minutes + ':' + seconds);
-      }, 1000);
+      }, 1000)
     }
-    countdown();
+    countdown()
+  }
+  else {
+    sessionStorage.clear()
   }
 })
 
